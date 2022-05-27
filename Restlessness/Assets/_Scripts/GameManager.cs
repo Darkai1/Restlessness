8a4cab736ef730057ba8db8b211de715c11c5bc9
Darkai1 <hernandosc19@hotmail.com>
2022-05-27 10:33:51 -0500

    cosas

--- Restlessness/Assets/_Scripts/GameManager.cs
+++ Restlessness/Assets/_Scripts/GameManager.cs
@@ -6,7 +6,11 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public bool gameOver = false;
-
+    [SerializeField]
+    private int _runestowin = 0;
+    public bool winner = false;
+    public GameObject screenWinner;
+    public GameObject screenLoser;
     public void Awake()
     {
          Instance= this;
@@ -15,7 +19,44 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
           gameOver = true;  
-    }
+    }          
 
+    public void playerwinner(int ObjectToWin)
+    {
+        if(gameOver==false)
+        {
+            if(_runestowin<=3)
+            {
+                _runestowin=_runestowin+ObjectToWin;
+                if(_runestowin==3)
+                {
+                    winner=true;
+                    InterfaceManager();
+                    gameOver = true;
+
+                }
+                
+            }
+        }
+    }
+    public void PlayerLose(bool gameStatus)
+    {
+        if(gameOver==false && gameStatus==true)
+        {
+            gameOver=gameStatus;
+            InterfaceManager();
+        }
+    }
+    public void InterfaceManager()
+    {
+        if(winner==true)
+        {
+            screenWinner.SetActive(true);
+        }
+        if (gameOver==true)
+        {
+            screenLoser.SetActive(true);
+        }
+    }
 
 }
